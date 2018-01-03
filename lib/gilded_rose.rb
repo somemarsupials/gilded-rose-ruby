a2187ca9ae903b5b13dda57f36038a95eaedbfae
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-01-03 15:24:15 +0000

    refactor GuildedRose, remove texttest_fixture

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,58 +1,18 @@
-
+require File.join(File.dirname(__FILE__), 'typed_factory')
 
 class GildedRose
   attr_reader :items
 
-  def initialize(items)
-    @items = items.map { |item| TypedItem.new(item) }
+  def initialize(items, factory = TypedFactory.new)
+    @items = convert(items, factory)
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
+  def update
+    @items.each { |item| item.update }
+  end
+
+  def convert(items, factory)
+    items.map { |item| factory.generate(item) }
   end
 end
 
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,4 +1,47 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'gilded_rose')
 
 describe GildedRose do
+  let(:typed_item) { double(:typed_item) }
+  let(:item) { double(:item) }
+  let(:factory) { double(:typed_factory, generate: typed_item) }
+  subject { described_class.new([item] * 5, factory) }
+
+  describe '#initialize' do
+    before(:each) do
+      allow_any_instance_of(described_class)
+        .to receive(:convert).and_return([typed_item])
+    end
+
+    describe 'when created' do
+      it 'converts items' do
+        expect(subject.items).to eq [typed_item]
+      end
+    end
+  end
+
+  describe '#update' do
+    before(:each) do
+      allow_any_instance_of(described_class)
+        .to receive(:convert).and_return([typed_item] * 4)
+    end
+
+    describe 'when updating' do
+      after(:each) { subject.update }
+
+      it 'calls update on each item' do
+        expect(typed_item).to receive(:update).exactly(4).times
+      end
+    end
+  end
+  
+  describe '#convert' do
+    describe 'when converting items' do
+      subject { described_class.new([], factory) }
+      after(:each) { subject.convert([item] * 3, factory) }
+      
+      it 'passes each item to the factory' do
+        expect(factory).to receive(:generate).with(item).exactly(3).times
+      end
+    end
+  end
 end
